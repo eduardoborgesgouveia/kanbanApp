3a8184e40d760e1b4d817f024980b09fdb01ade1
Eduardo Borges Gouveia <eduardo.gouveia@ltad.com.br>
2022-03-20 10:37:11 -0300

    commit inicial do backend do desafio Let's Code

---
+++ KanbanApp/KanbanApp/Filters/LogFilter.cs
@@ -0,0 +1,48 @@
+﻿using KanbanApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanbanApp.Filters
+{
+    public class LogFilter : IActionFilter
+    {
+
+        private readonly ApiContext _context;
+        private Cards currentCard { get; set; }
+
+        public LogFilter(ApiContext context)
+        {
+            _context = context;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            //Buscar o card que será alterado/removido
+            object obId;
+            context.RouteData.Values.TryGetValue("id", out obId);
+            if (obId != null)
+            {
+                string id = obId.ToString();
+                currentCard = _context.Cards.FirstOrDefault(s => s.id.ToString() == id);
+
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //Realizar o registro do log
+            if (context.Result.GetType() == typeof(OkObjectResult) && currentCard != null)
+            {
+                HttpRequest req = context.HttpContext.Request;
+                string methodType = req.Method == "PUT" ? "Alterar" : "Remover";
+                string log_string = string.Format("{0} - Card {1} - {2} - {3}", DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"), currentCard.id, currentCard.titulo, methodType);
+                System.Diagnostics.Debug.WriteLine(log_string);
+            }
+        }
+    }
+}
---
+++ KanbanApp/KanbanApp/Models/UserToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanbanApp.Models
+{
+    public class UserToken
+    {
+        public string token { get; set; }
+        public DateTime expirationDate { get; set; }
+    }
+}
